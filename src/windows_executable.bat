ad1730b02da3ff69ef5c305713c06f48eb6eb905
Kyser <kmontalvo3@gatech.edu>
2021-04-18 23:12:47 -0400

    opens up browser

--- src/windows_executable.bat
+++ src/windows_executable.bat
@@ -1,4 +1,5 @@
 npm install && ^
 npm install tailwindcss@npm:@tailwindcss/postcss7-compat @tailwindcss/postcss7-compat postcss@^7 autoprefixer@^9 && ^
 npm audit fix && ^
+START Http://localhost:8080 && ^
 npm run serve
